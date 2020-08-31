07af6f0f915d3af6fdff4d1914898cf8f53d2b10
mJordan Brady <hi@mjordan.codes>
2020-08-30 23:05:30 -0600

    A quick tour of gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Jordan's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Jordan Brady`,
+      summary: `who is a creative front end developer.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `mjordancodes`,
     },
   },
   plugins: [
